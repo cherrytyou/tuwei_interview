389e5a3e4eba169a7076ac80e42a05d8436e228a
cherrytyou <git>
2024-12-06 14:41:59 +0800

    Add test case for the G123 game

--- GameTestSelenium.py
+++ GameTestSelenium.py
@@ -1,12 +1,54 @@
-import os
-import shutil
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-# 指定驱动目标位置
-folder_path = r'C:\Users\Abby\PycharmProjects\pythonProject\venv\Scripts'
-file_path = os.path.join(folder_path, 'chromedriver.exe')
-download_driver_path = ChromeDriverManager().install()
-shutil.copy(download_driver_path, folder_path)
-driver = webdriver.Chrome(service=Service(file_path))
-driver.get("https://h5.g123.jp/game/jya?lang=zh-TW")
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+import time
+
+# 设置 Chrome 选项
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # 无头模式
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("window-size=1920,1080")
+
+# 创建 WebDriver 实例
+driver = webdriver.Chrome(options=chrome_options)
+
+try:
+    # 打开游戏网页
+    driver.get("https://h5.g123.jp/game/jya?lang=zh-TW")
+
+    # 等待页面加载
+    time.sleep(5)
+
+    # 查找并打印页面标题
+    page_title = driver.title
+    print("页面标题：", page_title)
+
+    # 检查标题是否与预期匹配
+    expected_title = "小邪神飛踢 混沌 | 開始遊戲 - G123"
+    if page_title == expected_title:
+        print("页面标题匹配！")
+    else:
+        print("页面标题不匹配！")
+
+        # 查找并点击开始游戏按钮
+    # start_button = driver.find_element(By.XPATH, '//button[contains(text(),"開始遊戲")]')  # 根据实际文本修改
+    # print("找到的按钮文本：", start_button.text)
+    #
+    # # 点击按钮
+    # start_button.click()
+    #
+    # # 等待游戏加载
+    # time.sleep(10)  # 根据实际情况进行调整
+    #
+    # # 进一步与游戏元素交互
+    # # 示例: 可以查找游戏中的元素并进行交互
+    #
+    # # 等待观察结果
+    # time.sleep(3)
+
+finally:
+    # 关闭浏览器
+    driver.quit()
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # tuwei_interview
+容器化：使用 Docker 创建你的测试工具镜像，确保包含所有运行自动化测试所需的依赖项。
+在测试机上安装docker:https://docs.docker.com/desktop/setup/install/windows-install/#install-docker-desktop-on-windows
+在docker启动的情况下:然后执行如下命令启动selenium执行服务端docker容器,运行 docker run -d --name selenium-standalone-chrome -h selenium-standalone-chrome --memory 1g --memory-swap -1 -p 4444:4444 selenium/standalone-chrome
+
+1. 打开命令提示符
+确保在 Windows 中打开命令提示符或 PowerShell，并导航到您的工作目录：
+cd C:\Users\Abby\Downloads\tuwei\tuwei_interview
+
+2. 构建 Docker 镜像
+运行以下命令来构建 Docker 镜像，命名为 my-selenium-app：
+docker build -t my-selenium-app .
+
+3. 启动 Docker 容器
+运行以下命令来启动 Docker 容器并执行 Selenium 脚本：
+docker run --privileged my-selenium-app
+
+请将：GameTestSelenium.py 文件中的ip192.168.137.1 地址改成脚本需要运行在的虚拟机ip地址
